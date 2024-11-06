c3951a63531f5db3f1c20c6a8300888273e833df
Farid Mammadli <ferid-memmedli@outlook.com>
2024-11-06 10:01:57 +0400

    fibonacci, palindrome, smallestEven

---
+++ src/Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int reversed = 0;
+        int remainingValue = x;
+        while (remainingValue != 0) {
+            reversed = (remainingValue % 10) + reversed * 10;
+            remainingValue /= 10;
+        }
+        return reversed == x;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(isPalindrome(n));
+    }
+}
---
+++ src/SmallestEven.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SmallestEven {
+    public static int smallestEvenMultiple(int n) {
+        return (n % 2 == 0) ? n : n * 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(smallestEvenMultiple(n));
+    }
+}
